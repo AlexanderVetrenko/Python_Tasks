d9572fe1ac43e3e967bd967b90bb9a0c700a9390
Alexander Vetrenko <vetrenko.ag@gmail.com>
2022-04-24 20:08:43 +0500

    Task16 решено

--- Task16.py
+++ Task16.py
@@ -1,7 +1,18 @@
 # Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму.
 
+def Sequence_Sum(x):
+    list1 = []
+    summ = 0.0
+    k = 1
+    for i in range(x):
+        item = (1+1/k)**k
+        list1.append(item)
+        summ += list1[i]
+        k += 1
+    return summ
 
-def Sequence(n):
 
-
-a = int(input('Введите количество членов последовательности '))
+print('Введите количество членов последовательности ')
+print('(1 + 1/n)^n')
+a = int(input())
+print(f'Сумма чисел последовательности равна {round(Sequence_Sum(a),2)}')
