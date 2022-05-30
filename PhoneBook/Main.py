1d1e27cd76d17849df34da3510dcc1f7913a9727
Alexander <vetrenko.ag@gmail.com>
2022-05-30 13:32:57 +0500

    Допилил телефонный справочник

--- PhoneBook/Main.py
+++ PhoneBook/Main.py
@@ -6,5 +6,16 @@
 #Фамилия_2,Имя_2,Телефон_2,Описание_2
 
 import ContrDB as CDB
+import ContrFind as CF
 
-CDB.DB_Add()
+while True:
+    print('===========================================')
+    print('Выберите режим работы со справочником')
+    print('1 - Создание новой записи')
+    print('2 - Поиск по фамилии')
+    print('3 - Вывести на экран все записи')
+    k = int(input())
+    if k ==1: CDB.DB_Add()
+    elif k ==2: CF.Fnd_Smbd(k)
+    elif k ==3: CF.Fnd_Smbd(k)
+    else: print('Введите 1, 2 или 3')
